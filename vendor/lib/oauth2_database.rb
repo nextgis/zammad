6e02c0fa769c456d7c75a3c472e0dc12797c33f9
Dmitry Baryshnikov <bishop.dev@gmail.com>
2019-10-20 01:34:51 +0300

    Add my.nextgis.com auth support

--- vendor/lib/oauth2_database.rb
+++ vendor/lib/oauth2_database.rb
@@ -10,5 +10,31 @@ class Oauth2Database < OmniAuth::Strategies::OAuth2
     args[2][:client_options] = args[2][:client_options].merge(config.symbolize_keys)
     super
   end
+  
+  def callback_url
+    full_host + script_name + callback_path
+  end
+
+  uid { raw_info['nextgis_guid'] }
+
+  info do
+    {
+      email: raw_info['email'],
+      username: raw_info['first_name'] + ' ' + raw_info['last_name'],
+      login: raw_info['email'],
+      first_name: raw_info['first_name'],
+      last_name: raw_info['last_name'],
+    }
+  end
+
+  extra do
+    {
+      'raw_info' => raw_info
+    }
+  end
+
+  def raw_info
+    @raw_info ||= access_token.get('/api/v1/user_info/').parsed
+  end
 
 end
